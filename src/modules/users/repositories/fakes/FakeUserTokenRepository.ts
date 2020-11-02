7d4dfec2441a2c72c84b6cc7f205ed9b685723fa
alexandre-roa <alexandre.z.roa@gmail.com>
2020-11-02 14:44:46 -0300

    API: Send forgot password email

---
+++ src/modules/users/repositories/fakes/FakeUserTokenRepository.ts
@@ -0,0 +1,33 @@
+import { uuid } from 'uuidv4';
+import UserToken from '../../infra/typeorm/entities/UserToken';
+import IUserTokenRepository from '@modules/users/repositories/IUserTokenRepository';
+
+class FakeUserTokenRepository implements IUserTokenRepository {
+  private userTokens: UserToken[] = [];
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      id: uuid(),
+      token: uuid(),
+      user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token,
+    );
+
+    return userToken;
+  }
+}
+
+export default FakeUserTokenRepository;
