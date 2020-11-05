65c40f59806f5870c03d6b9e1d6c3082f6057dcb
alexandre-roa <alexandre.z.roa@gmail.com>
2020-11-05 09:25:54 -0300

    API - List Cars by Value Range with Specs

--- src/modules/cars/services/ListAllCarsByRangeValueService.ts
+++ src/modules/cars/services/ListAllCarsByRangeValueService.ts
@@ -2,20 +2,32 @@ import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import ICarsRepository from '../repositories/ICarsRepository';
+import ICarSpecsRepository from '../repositories/ICarSpecsRepository';
 
 import Car from '../infra/typeorm/entities/Car';
+import CarSpec from '../infra/typeorm/entities/CarSpec';
 
 @injectable()
 class ListAllCarsByRangeValueService {
   constructor(
     @inject('CarsRepository')
     private carsRepository: ICarsRepository,
+
+    @inject('CarSpecsRepository')
+    private carSpecsRepository: ICarSpecsRepository,
   ) {}
 
   public async execute(from: number, to: number): Promise<Car[]> {
     const cars = await this.carsRepository.findByValueRange(from, to);
 
-    return cars;
+    const carsWithSpecs = Promise.all(
+      cars.map(async car => ({
+        ...car,
+        specs: await this.carSpecsRepository.findByCarId(car.id),
+      })),
+    );
+
+    return carsWithSpecs;
   }
 }
 
