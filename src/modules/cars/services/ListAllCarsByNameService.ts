848acd1a78f4e0aaed1fe350effc8f8bca1f9505
alexandre-roa <alexandre.z.roa@gmail.com>
2020-11-05 09:23:16 -0300

    API - List Cars by name with specs

--- src/modules/cars/services/ListAllCarsByNameService.ts
+++ src/modules/cars/services/ListAllCarsByNameService.ts
@@ -2,20 +2,34 @@ import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import ICarsRepository from '../repositories/ICarsRepository';
+import ICarSpecsRepository from '../repositories/ICarSpecsRepository';
 
 import Car from '../infra/typeorm/entities/Car';
+import CarSpec from '../infra/typeorm/entities/CarSpec';
+
+interface IResponse {
+  car: Car;
+  specs: CarSpec[];
+}
 
 @injectable()
 class ListAllCarsByNameService {
   constructor(
     @inject('CarsRepository')
     private carsRepository: ICarsRepository,
+
+    @inject('CarSpecsRepository')
+    private carSpecsRepository: ICarSpecsRepository,
   ) {}
 
-  public async execute(name: string): Promise<Car> {
+  public async execute(name: string): Promise<IResponse> {
     const car = await this.carsRepository.findByName(name);
 
-    return car;
+    const carSpecs = await this.carSpecsRepository.findByCarId(car.id);
+
+    const carWithSpecs = { car, specs: carSpecs };
+
+    return carWithSpecs;
   }
 }
 
